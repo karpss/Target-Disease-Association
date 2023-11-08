2865bb8cb76b58847f826f9819b8574aa4cc70ba
karpss <ola.fam10@gmail.com>
2023-11-08 12:48:17 +0100

    Refactored Chart tab switching to use useState instead of direct DOM manipulation

--- src/components/ChartTab/ChartTab.tsx
+++ src/components/ChartTab/ChartTab.tsx
@@ -1,4 +1,4 @@
-import React, { MouseEvent } from 'react';
+import React, { useState } from 'react';
 import '../../styles/ChartTabStyles.css';
 import BarChart from '../../charts/BarChart';
 import RadarChart from '../../charts/RadarChart';
@@ -10,63 +10,48 @@ interface ChartTabProps {
 }
 
 function ChartTab({ index, datatypeScores, title }: ChartTabProps) {
-  // eslint-disable-next-line @typescript-eslint/no-shadow
-  const openTab = (event: MouseEvent, tabName: string, index: number) => {
-    const tabContentElements = document.getElementById(
-      `tabContentWrapper${index}`
-    )?.children;
-    const tabLinkElements = document.getElementById(
-      `tablinksWrapper${index}`
-    )?.children;
-    for (let i = 0; i < (tabLinkElements?.length || 0); i += 1) {
-      if (
-        tabLinkElements &&
-        tabLinkElements[i].getAttribute('id') === tabName + index
-      ) {
-        tabLinkElements[i].classList.add('active');
-      } else if (tabLinkElements) {
-        tabLinkElements[i].classList.remove('active');
-      }
-    }
-    for (let i = 0; i < (tabContentElements?.length || 0); i += 1) {
-      if (
-        tabContentElements &&
-        tabContentElements[i].getAttribute('id') === tabName + index
-      ) {
-        tabContentElements[i].classList.add('tabVisibility');
-      } else if (tabContentElements) {
-        tabContentElements[i].classList.remove('tabVisibility');
-      }
-    }
+  const [activeTab, setActiveTab] = useState('Bar');
+
+  const openTab = (tabName: string) => {
+    setActiveTab(tabName);
   };
+
   return (
     <div className="tabWrapper">
       <div id={`tablinksWrapper${index}`} className="tab">
         <button
           id={`Bar${index}`}
-          className="tablinks active"
-          onClick={(e) => openTab(e, 'Bar', index)}
+          className={`tablinks ${activeTab === 'Bar' ? 'active' : ''}`}
+          onClick={() => openTab('Bar')}
           type="button"
         >
           Bar chart
         </button>
         <button
           id={`Radar${index}`}
-          className="tablinks"
-          onClick={(e) => openTab(e, 'Radar', index)}
+          className={`tablinks ${activeTab === 'Radar' ? 'active' : ''}`}
+          onClick={() => openTab('Radar')}
           type="button"
         >
           Radar chart
         </button>
       </div>
       <div id={`tabContentWrapper${index}`}>
-        <div id={`Bar${index}`} className="tabcontent tabVisibility">
+        <div
+          id={`Bar${index}`}
+          className={`tabcontent ${activeTab === 'Bar' ? 'tabVisibility' : ''}`}
+        >
           <div className="graphStyles">
             <BarChart datatypeScores={datatypeScores} title={title} />
           </div>
         </div>
 
-        <div id={`Radar${index}`} className="tabcontent">
+        <div
+          id={`Radar${index}`}
+          className={`tabcontent ${
+            activeTab === 'Radar' ? 'tabVisibility' : ''
+          }`}
+        >
           <div className="graphStyles" style={{ width: '500px' }}>
             <RadarChart datatypeScores={datatypeScores} title={title} />
           </div>
